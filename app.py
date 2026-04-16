2a70f337bb6c8917c23f59663a59e59fb7583501
Uliana <uemakarove@outlook.com>
2026-04-16 14:30:00 +0100

    Some issue fixes

--- app.py
+++ app.py
@@ -114,5 +114,5 @@ app.layout = html.Div(
 
 if __name__ == "__main__":
     # Dash dev server.
-    app.run_server(debug=True, host="0.0.0.0", port=8050)
+    app.run(debug=True, host="0.0.0.0", port=8050)
 
